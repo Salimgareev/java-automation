c3675bc1d49a022c18ebfee32d4d9241daf04335
Kamil <kamilsalimgareev270@gmail.com>
2024-01-11 17:55:35 +0500

    Создан класс менеджер для страниц

---
+++ src/main/java/org/example/managers/PageManager.java
@@ -0,0 +1,47 @@
+package org.example.managers;
+
+import org.example.pages.HomePage;
+
+public class PageManager {
+    /**
+     * Менеджер страничек
+     */
+    private static PageManager pageManager;
+
+    /**
+     * Стартовая страничка
+     */
+    private HomePage homePage;
+
+    /**
+     * Конструктор специально был объявлен как private (singleton паттерн)
+     *
+     * @see PageManager#getPageManager()
+     */
+    private PageManager() {
+    }
+
+    /**
+     * Ленивая инициализация PageManager
+     *
+     * @return PageManager
+     */
+    public static PageManager getPageManager() {
+        if (pageManager == null) {
+            pageManager = new PageManager();
+        }
+        return pageManager;
+    }
+
+    /**
+     * Ленивая инициализация {@link HomePage}
+     *
+     * @return StartPage
+     */
+    public HomePage getHomePage() {
+        if (homePage == null) {
+            homePage = new HomePage();
+        }
+        return homePage;
+    }
+}
