bc01b317e70922eadf2968da7ee428471e29822a
Kamil <kamilsalimgareev270@gmail.com>
2024-01-11 17:57:20 +0500

    Создан базовый класс для страниц

---
+++ src/main/java/org/example/pages/BasePage.java
@@ -0,0 +1,125 @@
+package org.example.pages;
+
+import org.example.managers.DriverManager;
+import org.example.managers.PageManager;
+import org.example.managers.TestPropManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+    /**
+     * Менеджер WebDriver
+     *
+     * @see DriverManager#getDriverManager()
+     */
+    protected final DriverManager driverManager = DriverManager.getDriverManager();
+
+    /**
+     * Менеджер страничек
+     *
+     * @see PageManager
+     */
+    protected PageManager pageManager = PageManager.getPageManager();
+
+
+    /**
+     * Объект для имитации реального поведения мыши или клавиатуры
+     *
+     * @see Actions
+     */
+    protected Actions action = new Actions(driverManager.getDriver());
+
+
+    /**
+     * Объект для выполнения любого js кода
+     *
+     * @see JavascriptExecutor
+     */
+    protected JavascriptExecutor js = (JavascriptExecutor) driverManager.getDriver();
+
+
+    /**
+     * Объект явного ожидания
+     * При применении будет ожидать заданного состояния 10 секунд с интервалом в 1 секунду
+     *
+     * @see WebDriverWait
+     */
+
+    protected WebDriverWait wait = new WebDriverWait(driverManager.getDriver(), Duration.ofSeconds(10));
+
+
+    /**
+     * Менеджер properties
+     *
+     * @see TestPropManager#getTestPropManager()
+     */
+    private final TestPropManager props = TestPropManager.getTestPropManager();
+
+
+    /**
+     * Конструктор позволяющий инициализировать все странички и их элементы помеченные аннотацией {@link FindBy}
+     * Подробнее можно просмотреть в класс {@link org.openqa.selenium.support.PageFactory}
+     *
+     * @see FindBy
+     * @see PageFactory
+     */
+    public BasePage() {
+        PageFactory.initElements(driverManager.getDriver(), this);
+    }
+
+
+    /**
+     * Функция позволяющая производить scroll до любого элемента с помощью js
+     *
+     * @param element - веб-элемент странички
+     * @see JavascriptExecutor
+     */
+    protected WebElement scrollToElementJs(WebElement element) {
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+        return element;
+    }
+
+
+    /**
+     * Явное ожидание состояния clickable элемента
+     *
+     * @param element - веб-элемент который требует проверки clickable
+     * @return WebElement - возвращаем тот же веб элемент что был передан в функцию
+     * @see WebDriverWait
+     * @see org.openqa.selenium.support.ui.FluentWait
+     * @see org.openqa.selenium.support.ui.Wait
+     * @see ExpectedConditions
+     */
+    protected WebElement waitUtilElementToBeClickable(WebElement element) {
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    /**
+     * Явное ожидание того что элемент станет видимым
+     *
+     * @param element - веб элемент который мы ожидаем что будет  виден на странице
+     */
+    protected WebElement waitUtilElementToBeVisible(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+
+    /**
+     * Общий метод по заполнения полей ввода
+     *
+     * @param field - веб-элемент поле ввода
+     * @param value - значение вводимое в поле
+     */
+    protected void fillInputField(WebElement field, String value) {
+        scrollToElementJs(field);
+        waitUtilElementToBeClickable(field).click();
+        field.sendKeys(value);
+    }
+}
