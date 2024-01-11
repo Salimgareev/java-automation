a790ca9294dc07bc08d54b251e4c75c76a7b4618
Kamil <kamilsalimgareev270@gmail.com>
2024-01-11 19:45:19 +0500

    Добавлен метод проверки наличия определенного заголовка в таблице товаров

--- src/main/java/org/example/pages/ProductsPage.java
+++ src/main/java/org/example/pages/ProductsPage.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class ProductsPage extends BasePage{
     @FindBy(xpath = "//h5[contains(text(),'Список товаров')]")
     private WebElement title;
+
+    @FindBy(xpath = "//table[@class = 'table']/thead/*/*")
+    private List<WebElement> tableHeaders;
 
     /**
      * Проверка открытия страницы, путём проверки title страницы
@@ -19,4 +24,22 @@ public class ProductsPage extends BasePage{
         System.out.println("Заголовок: " + title.getText());
         return this;
     }
+
+    /**
+     * Проверка наличия опр заголовка в таблице товаров
+     *
+     * @return ProductsPage - т.е. остаемся на этой странице
+     */
+    public ProductsPage checkAvailabilityHeadings(String nameHeader) {
+        for (WebElement header: tableHeaders) {
+            if (header.getText().trim().equalsIgnoreCase(nameHeader)){
+                System.out.println("Заголовок таблицы: " + header.getText().trim());
+                return this;
+            }
+        }
+
+        Assertions.fail("Меню '" + nameHeader + "' не было найдено на странице товаров!");
+        return this;
+    }
+
 }
