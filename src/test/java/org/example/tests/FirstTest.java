5678d475ccd7957f99c735ef21b7c2b2a6ff905f
Kamil <kamilsalimgareev270@gmail.com>
2024-01-11 18:03:04 +0500

    Создан первый тестовый класс, начальный вариант

---
+++ src/test/java/org/example/tests/FirstTest.java
@@ -0,0 +1,13 @@
+package org.example.tests;
+
+import org.example.basetestsclass.BaseTests;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest extends BaseTests {
+    @Test
+    public void startTest() {
+        app.getHomePage()
+                .selectBaseMenu("Песочница");
+//                .selectSubMenu("Путешествия");
+    }
+}
