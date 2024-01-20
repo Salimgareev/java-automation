5e625c091b0493a47d371e5466ba968ad01ac146
Kamil <kamilsalimgareev270@gmail.com>
2024-01-20 20:17:41 +0500

    Добавлены новые константы

--- src/main/java/org/example/utils/PropConst.java
+++ src/main/java/org/example/utils/PropConst.java
@@ -3,6 +3,10 @@ package org.example.utils;
 public class PropConst {
     public static final String BASE_URL = "base.url";
 
+    public static final String DATABASE_URL = "database.url";
+
+    public static final String SELENOID_URL = "selenoid.url";
+
     public static final String TYPE_BROWSER = "type.browser";
 
     public static final String PATH_CHROME_DRIVER_WINDOWS = "path.chrome.driver.windows";
@@ -12,4 +16,6 @@ public class PropConst {
     public static final String IMPLICITLY_WAIT = "implicitly.wait";
 
     public static final String PAGE_LOAD_TIMEOUT = "page.load.timeout";
+
+    public static final String TYPE_DRIVER = "type.driver";
 }
